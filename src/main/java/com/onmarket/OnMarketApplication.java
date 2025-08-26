2d3bfab2bfa4302a0b0eaba030ef36f84b9c117e
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-08-26 23:24:44 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/onmarket/OnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.onmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onmarket/OnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
