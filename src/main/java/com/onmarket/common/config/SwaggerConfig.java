1e61be1343ea4c4d8de3c58bd0053ec82be52e78
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2025-08-27 16:55:31 +0900

    feat: swagger 세팅

---
+++ src/main/java/com/onmarket/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.onmarket.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI openAPI(){
+
+//        SecurityScheme securityScheme = new SecurityScheme()
+//                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+//                .in(SecurityScheme.In.HEADER).name("Authorization");
+//        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(apiInfo());
+//                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+//                .security(Arrays.asList(securityRequirement));
+    }
+    private Info apiInfo(){
+        return new Info()
+                .title("ONMarket API Document")
+                .version("0.0.1")
+                .description("ONMarket의 API 명세서입니다.");
+    }
+
+}
