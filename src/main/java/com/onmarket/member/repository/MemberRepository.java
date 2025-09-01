80119cd549b754f60de61f01801afdcea2bc0ea0
glorypnag <dys1128@naver.com>
2025-09-01 12:09:26 +0900

    fix: 동명이인 회원가입 시 차단 문제 해결

--- src/main/java/com/onmarket/member/repository/MemberRepository.java
+++ src/main/java/com/onmarket/member/repository/MemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
